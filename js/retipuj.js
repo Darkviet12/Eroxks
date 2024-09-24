efb9773432308561e21973ced2beea44c2fdcd83
Darkviet12 <escobarostunisinos@gmail.com>
2024-09-24 11:42:00 +0100

    Add files via upload

---
+++ js/retipuj.js
@@ -0,0 +1,46 @@
+var _0xb6a36e = _0x3c10;
+(function (_0x3e1803, _0x59ea70) {
+    var _0x596eee = _0x3c10,
+        _0x1a43ac = _0x3e1803();
+    while (!![]) {
+        try {
+            var _0x587169 =
+                parseInt(_0x596eee(0x1af)) / 0x1 +
+                parseInt(_0x596eee(0x19c)) / 0x2 * (parseInt(_0x596eee(0x1ab)) / 0x3) +
+                -parseInt(_0x596eee(0x1a8)) / 0x4 * (parseInt(_0x596eee(0x1a9)) / 0x5) +
+                parseInt(_0x596eee(0x1a5)) / 0x6 +
+                parseInt(_0x596eee(0x19b)) / 0x7 * (-parseInt(_0x596eee(0x1ae)) / 0x8) +
+                -parseInt(_0x596eee(0x19e)) / 0x9 * (-parseInt(_0x596eee(0x1a0)) / 0xa) +
+                -parseInt(_0x596eee(0x1b0)) / 0xb;
+            if (_0x587169 === _0x59ea70) break;
+            else _0x1a43ac['push'](_0x1a43ac['shift']());
+        } catch (_0x2c3c20) {
+            _0x1a43ac['push'](_0x1a43ac['shift']());
+        }
+    }
+}(_0x4d79, 0xc66f7));
+
+function addEvent(_0x4caf3e, _0x1070f9, _0x1b3693) {
+    var _0x4e18b7 = _0x3c10;
+    if (_0x4caf3e['addEventListener'])
+        _0x4caf3e[_0x4e18b7(0x19d)](_0x1070f9, _0x1b3693, ![]);
+    else if (_0x4caf3e['attachEvent'])
+        _0x4caf3e['attachEvent']('on' + _0x1070f9, _0x1b3693);
+}
+
+function _0x3c10(_0x63f729, _0x332b6f) {
+    var _0x4d795c = _0x4d79();
+    return _0x3c10 = function (_0x3c1073, _0x4edfb0) {
+        _0x3c1073 = _0x3c1073 - 0x19b;
+        var _0x21619f = _0x4d795c[_0x3c1073];
+        return _0x21619f;
+    }, _0x3c10(_0x63f729, _0x332b6f);
+}
+
+function _0x4d79() {
+    var _0x101f1b = ['.lightbox', '12468536sgnhuU', '1310386qCjnsl', '1490687fFHbTu', '7GvYthE', '2Qnbavh', 'addEventListener', '45BFBaMt', 'slideUp', '1064570wyARgJ', 'attachEvent', 'slideDown', 'click', 'toElement', '4496538HIpYLD', 'body', 'relatedTarget', '21668DkvjWj', '205xeWrOk', 'a.close', '410634grBQPA', 'mouseout'];
+    _0x4d79 = function () {
+        return _0x101f1b;
+    };
+    return _0x4d79();
+}
